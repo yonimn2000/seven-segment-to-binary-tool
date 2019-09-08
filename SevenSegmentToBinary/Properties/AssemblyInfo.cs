5adf564b832ca3f27f3530750d3a066bd9dfcfd3
Yonatan Mankovich <yonimn2000@gmail.com>
2019-09-07 20:39:08 -0400

    Updated project metadata.

--- SevenSegmentToBinary/Properties/AssemblyInfo.cs
+++ SevenSegmentToBinary/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("7 Segment to Binary")]
+[assembly: AssemblyTitle("YonatanMankovich.SevenSegmentToBinary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("7 Segment to Binary")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Yonatan Mankovich")]
+[assembly: AssemblyProduct("YonatanMankovich.SevenSegmentToBinary")]
+[assembly: AssemblyCopyright("© 2017 Yonatan Mankovich")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
